b90615da3f1d53cfd4c3c62ac5193137be628804
Mubashir-ali-baig <mubashirbaig98@gmail.com>
2020-10-05 22:04:09 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './App.css';
+import Cipher from './components/ciphers';
+import Selector from './components/selector'
+function App() {
+  return (
+    <div className="App">
+      <h1>Made By Mubashir Ali Baig</h1>
+      <h2>Seat No# B16101075</h2>
+      <h3>Section: A</h3>
+      <Selector />
+    </div>
+  );
+}
+
+export default App;
